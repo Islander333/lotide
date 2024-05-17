95d453be8b9e7108889f2ca5a145c206e859997d
Kyle Lindsay <kyle.lindsay333@gmail.com>
2024-05-16 22:00:14 -0700

    add tail function that returns array without first element

--- head.js
+++ head.js
@@ -9,9 +9,6 @@ const assertEqual = function(actual, expected) {
 
 };
 
-//test code
-assertEqual("Lighthouse Labs", "mista white");
-assertEqual(1, 1);
 
 
 //function implementation
@@ -20,6 +17,3 @@ const head = function(arr) {
   return arr[0];
 };
 
-//test code
-
-console.log(head([1]));
