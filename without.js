03f500d81098d10c7354ab15f932bb4a92172463
Kyle Lindsay <kyle.lindsay333@gmail.com>
2024-05-19 09:52:56 -0700

    make function that compares a ssource and a itemsToRemove array as parameters and returns new array without itemsToRemove elements

---
+++ without.js
@@ -0,0 +1,26 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArrayEquals = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion failed: ${array1} !== ${array2}`)
+  }
+};
+
+const without = function(source, itemsToRemove) {
+  return source.filter(function(item) {
+    return !itemsToRemove.includes(item);
+  });
+};
+
