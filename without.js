8c7c2fd2aa284772ee0f7915449cdc918e646e0f
Kyle Lindsay <kyle.lindsay333@gmail.com>
2024-05-19 10:17:05 -0700

    update code to compass requirements

--- without.js
+++ without.js
@@ -38,3 +38,6 @@ without(words, ["lighthouse"]); // no need to capture return value for this test
 // Make sure the original array was not altered by the without function
 assertArraysEqual(words, ["hello", "world", "lighthouse"]);
 
+
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]); // => should PASS
+
