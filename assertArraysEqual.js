becde7bba43e34bc40f04ddf6fca800adfaef994
Kyle Lindsay <kyle.lindsay333@gmail.com>
2024-05-18 21:23:54 -0700

    change to correct file name

---
+++ assertArraysEqual.js
@@ -0,0 +1,35 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArrayEquals = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion failed: ${array1} !== ${array2}`)
+  }
+};
+
+
+//test code
+assertArrayEquals([1, 2, 3], [1, 2, 3]);
+assertArrayEquals([1, 2, 3], [3, 2, 1]);
+assertArrayEquals(["1", "2", "3"], ["1", "2", "3"]);
