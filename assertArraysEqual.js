3f7b126bc103e711e8adaa354bb52f2d35822916
Kyle Lindsay <kyle.lindsay333@gmail.com>
2024-06-04 18:29:35 -0700

    update assertArraysEqual and make test file for modularity

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,12 +1,3 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
 
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
@@ -20,7 +11,8 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-const assertArrayEquals = function(array1, array2) {
+
+const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
     console.log(`✅✅✅ Assertion passed: ${array1} === ${array2}`);
   } else {
@@ -28,8 +20,6 @@ const assertArrayEquals = function(array1, array2) {
   }
 };
 
+module.exports = assertArraysEqual;
 
-//test code
-assertArrayEquals([1, 2, 3], [1, 2, 3]);
-assertArrayEquals([1, 2, 3], [3, 2, 1]);
-assertArrayEquals(["1", "2", "3"], ["1", "2", "3"]);
+
