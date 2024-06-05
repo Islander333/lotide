8fa27e7889653ca9f407eb3526e1adb37b64fe23
Kyle Lindsay <kyle.lindsay333@gmail.com>
2024-06-04 17:09:45 -0700

    made test files to hold test code for assertEqual and Head

---
+++ test/assertEqualTest.js
@@ -0,0 +1,6 @@
+
+const assertEqual = require("../assertEqual");
+
+//test code
+assertEqual("Lighthouse Labs", "mista white");
+assertEqual(1, 1);
---
+++ test/headTest.js
@@ -0,0 +1,5 @@
+
+const head = require("../head");
+const assertEqual = require("../assertEqual");
+//head test code
+assertEqual(head([1,2,3]), 1);
