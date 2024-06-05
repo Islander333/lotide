20b63bcc9b4d665ba10d9a7e3ecbd4fb65873c92
Kyle Lindsay <kyle.lindsay333@gmail.com>
2024-06-04 17:57:25 -0700

    update test code to test if original arr remains unchanged after running tail

--- test/tailTest.js
+++ test/tailTest.js
@@ -2,6 +2,11 @@ const tail = require("../tail");
 const assertEqual = require("../assertEqual");
 
 // Test Case 1: Check the returned array elements
+const OGarray = [1,2,3,4];
+const ogResult = tail(OGarray);
+assertEqual(ogResult.length, 3);
+assertEqual(OGarray.length, 4);
+
 const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result.length, 2); // ensure we get back two elements
 assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
